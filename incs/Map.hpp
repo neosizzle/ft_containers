6816083157803e4145a88fd36baab158d4582861
neosizzle <junahnngd1115@gmail.com>
2022-01-19 21:52:27 +0800

    change fs

---
+++ incs/Map.hpp
@@ -0,0 +1,8 @@
+#ifndef __MAP__H
+#define __MAP__H
+#include "main.hpp"
+
+template <class Key, class T, class Compare=std::less<Key>, class Alloc = std::allocator<std::pair<const Key, T> > >
+
+
+#endif  //!__MAP__H
---
+++ incs/main.hpp
@@ -0,0 +1,7 @@
+#ifndef __MAIN__H
+#define __MAIN__H
+
+#include <utility>
+#include <functional>
+#include <memory>
+#endif  //!__MAIN__H
